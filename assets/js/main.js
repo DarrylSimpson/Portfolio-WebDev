e061e0c578cc84e8214e3437b0c62dced2302b0f
DarrylSimpson <darryl.simpson0793@gmail.com>
2022-03-13 22:30:12 -0500

    added frontend and backend skills, working on the accordion effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,10 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /* =====ACCORDION SKILLS ===== */
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+      
 
 /*===== QUALIFICATION TABS ===== */
 
