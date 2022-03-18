8d6959e73e7440da64f2f2346b68351fd260d983
DarrylSimpson <darryl.simpson0793@gmail.com>
2022-03-17 21:07:30 -0500

    added mock portfolio seciton need to fill in forreal

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,6 +49,20 @@ skillsHeader.forEach((el) =>{
 })
 
 
+/*===== PORTFOLIO SWIPER =====*/
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true.valueOf,
+    },
+  });
+
 
 /*===== QUALIFICATION TABS ===== */
 
